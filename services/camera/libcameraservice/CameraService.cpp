7403257408790628a71e4c3acaf0620b7360096e
betaxab <betaxab@gmail.com>
2023-08-17 21:37:47 +0800

    libcameraservice: Allow Moto Camera to skip invalid frames
    test: Take various photos in all modes
    Change-Id: Ib9231fe523caead1edbc699f06fda733a94cfb20
    Signed-off-by: betaxab <betaxab@gmail.com>

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -95,7 +95,6 @@ namespace {
 }; // namespace anonymous
 
 namespace android {
-
 using namespace camera3;
 using namespace camera3::SessionConfigurationUtils;
 
@@ -116,6 +115,7 @@ namespace flags = com::android::internal::camera::flags;
 // Use "adb shell dumpsys media.camera -v 1" to change it.
 volatile int32_t gLogLevel = 0;
 
+volatile bool MotoCamera = false;
 #define LOG1(...) ALOGD_IF(gLogLevel >= 1, __VA_ARGS__);
 #define LOG2(...) ALOGD_IF(gLogLevel >= 2, __VA_ARGS__);
 
--- services/camera/libcameraservice/CameraService.h
+++ services/camera/libcameraservice/CameraService.h
@@ -68,6 +68,7 @@
 namespace android {
 
 extern volatile int32_t gLogLevel;
+extern volatile bool MotoCamera;
 
 class MemoryHeapBase;
 class MediaPlayer;
